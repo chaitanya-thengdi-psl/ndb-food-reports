9afc33bf2fc5a192132664451e9955b5e7a0ba3f
Chaitanya Thengdi <chaitanya_thengdi@persistent.com>
2023-10-10 13:24:01 +0530

    Search query with results list and add to foods list button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import useFood from '../../hooks/useFood/useFood';
 
 import Breadcrumb from '../Breadcrumb/Breadcrumb';
@@ -8,24 +10,45 @@ import './App.scss';
 
 function App() {
 
-  function searchFoodList(query) {
-    console.log(`Searched for ${query}`)
-    fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=${query}`, {
-      method: 'GET'
-    }).then(response => response.json())
-      .then(data => console.log({ data }));
+  const [foods, setFoods] = useState([]);
+  const [query, setQuery] = useState(false);
+  const [searchData, setSearchData] = useState([])
+  const [viewingSearchResults, setViewingSearchResults] = useState(true);
 
+  useEffect(() => {
+    if(query) {
+      setViewingSearchResults(true);
+      fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=${query}`, {
+        method: 'GET'
+      })
+        .then(response => response.json())
+        .then(data => {
+          setSearchData(data.foods.slice(0, 5));
+        });
+      } else {
+        setViewingSearchResults(false);
+      }
+  }, [query]);
 
+  function addToFoodsList(value) {
+    if(!foods.map(food => food.fdcId).includes(value.fdcId)) {
+      setFoods([...foods, value])
+    }
   }
-
-  // called on each rerender, move to effect
-  const foods = useFood({foodId: 534358});
 
   return (
     <div className="App">
       <Breadcrumb />
       <h1>Food Reports</h1>
-      <Search onSearch={(query) => searchFoodList(query)}/>
+      <Search onSearch={(query) => setQuery(query)}/>
+      {viewingSearchResults && <div>
+        Search results:
+        <ul>
+          {searchData.map(datum => <li>
+            <span>{datum.description}</span> <button onClick={() => addToFoodsList(datum)}>Add</button>
+          </li>)}
+        </ul>
+      </div>}
       <FoodList foods={foods} />
     </div>
   );
--- src/components/FoodList/FoodList.js
+++ src/components/FoodList/FoodList.js
@@ -1,8 +1,9 @@
 function FoodList ({foods}) {
     return <div className="FoodList">
+        <p>Foods list:</p>
         <ul>
             {foods.map(food => <li>
-                {food.description} <a href="#">See Report</a>
+                {food.description} <a href="#">See Report</a> <a href="#">Favorite</a>
             </li>)}
         </ul>
     </div>
