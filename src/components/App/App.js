17c9b8ac8e8cce18b11dc3300398af8c487b1220
Chaitanya Thengdi <chaitanya_thengdi@persistent.com>
2023-10-10 12:16:33 +0530

    Add search API and lodash for throttle/debounce

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,6 +10,12 @@ function App() {
 
   function searchFoodList(query) {
     console.log(`Searched for ${query}`)
+    fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=${query}`, {
+      method: 'GET'
+    }).then(response => response.json())
+      .then(data => console.log({ data }));
+
+
   }
 
   // called on each rerender, move to effect
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,12 @@
+import {debounce} from 'lodash';
+
 function Search ({onSearch}) {
+
+    // calls onSearch only after 300ms each time the user types
+    const onChange = debounce(value => onSearch(value), 300);
+
     return <div className="Search">
-        <label>Search Foods:</label> <input type="search" id="food-search" placeholder="Search foods" onChange={e => onSearch(e.target.value)}/>
+        <label>Search Foods:</label> <input type="search" id="food-search" placeholder="Search foods" onChange={(e) => onChange(e.target.value)}/>
         <button>Search</button>
     </div>;
 }
